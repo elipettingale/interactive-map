818971d0b023c916a26f66400952bda134c33c91
Elliot Pettingale <elipettingale@gmail.com>
2021-04-18 08:06:06 +0100

    setup webpack

---
+++ dist/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Interactive Map</title>
+</head>
+<body>
+<h1>Interactive Map</h1>
+<script src="main.js"></script>
+</body>
+</html>

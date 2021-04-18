9146f68fffe6483c6269cad64df9a1ef3b02be2e
Elliot Pettingale <elipettingale@gmail.com>
2021-04-18 10:57:52 +0100

    import mapbox css via compiled stylesheet

--- dist/index.php
+++ dist/index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8" />
     <title>Interactive Map</title>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
 </head>
 <body>
 <main id="app" class="main"></main>

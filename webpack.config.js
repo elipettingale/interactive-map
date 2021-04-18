e8ea4b7044ab3ee56f6f077ff8ea2db1dc197f13
Elliot Pettingale <elipettingale@gmail.com>
2021-04-18 08:14:21 +0100

    add webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000,
+    },
     entry: './src/index.js',
     output: {
         filename: 'main.js',
